09375c5dff18d9137e3379d8ffb26a55ec39a09b
luongvanminh <luongvanminh>
2023-03-13 17:03:40 +0700

    final buoi 4

--- Program.cs
+++ Program.cs
@@ -5,34 +5,27 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<StoreDbContext>(opts =>
 {
   opts.UseSqlServer(
-  builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+      builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
-
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
-builder.Services.AddRazorPages();
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession();
+builder.Services.AddScoped<IOrderRepository, EFOrderRepository>();
 
+builder.Services.AddRazorPages(); builder.Services.AddDistributedMemoryCache(); builder.Services.AddSession();
+builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp)); builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 var app = builder.Build();
-
 app.UseStaticFiles();
 app.UseSession();
-
-app.MapControllerRoute("catpage", "{category}/Page{productPage:int}",
-new { Controller = "Home", action = "Index" });
-
+app.MapControllerRoute("catpage",
+    "{category}/Page{productPage:int}",
+    new { Controller = "Home", action = "Index" });
 app.MapControllerRoute("page", "Page{productPage:int}",
-new { Controller = "Home", action = "Index", productPage = 1 });
-
+    new { Controller = "Home", action = "Index", productPage = 1 });
 app.MapControllerRoute("category", "{category}",
-new { Controller = "Home", action = "Index", productPage = 1 });
-
+    new { Controller = "Home", action = "Index", productPage = 1 });
 app.MapControllerRoute("pagination",
-"Products/Page{productPage}",
-new { Controller = "Home", action = "Index", productPage = 1 });
-
+    "Products/Page{productPage}",
+    new { Controller = "Home", action = "Index", productPage = 1 });
 app.MapDefaultControllerRoute();
 app.MapRazorPages();
-
 SeedData.EnsurePopulated(app);
 app.Run();
