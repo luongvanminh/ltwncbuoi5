873da271b07bcadf89e3837ca01cc4caa3e38850
luongvanminh <luongvanminh>
2023-03-14 08:41:48 +0700

    format program.cs

--- Program.cs
+++ Program.cs
@@ -10,8 +10,12 @@ builder.Services.AddDbContext<StoreDbContext>(opts =>
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 builder.Services.AddScoped<IOrderRepository, EFOrderRepository>();
 
-builder.Services.AddRazorPages(); builder.Services.AddDistributedMemoryCache(); builder.Services.AddSession();
-builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp)); builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddRazorPages(); 
+builder.Services.AddDistributedMemoryCache(); 
+builder.Services.AddSession();
+builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp)); 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 var app = builder.Build();
 app.UseStaticFiles();
 app.UseSession();
@@ -27,5 +31,6 @@ app.MapControllerRoute("pagination",
     new { Controller = "Home", action = "Index", productPage = 1 });
 app.MapDefaultControllerRoute();
 app.MapRazorPages();
+
 SeedData.EnsurePopulated(app);
 app.Run();
